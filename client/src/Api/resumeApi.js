680ed8705450169383d7970f920e4208c5f26c00
anupreksha18 <intern.anu.negi>
2025-09-01 11:52:05 +0530

    Clean up tracked files and apply .gitignore

--- client/src/Api/resumeApi.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
- axios.defaults.withCredentials = true;
-
- export const getMyResume = () => axios.get("/api/resumes/me");
- export const createOrUpdateResume = (resumeData) => axios.post("/api/resumes", resumeData);
- export const deleteResume = () => axios.delete("/api/resumes/me");
